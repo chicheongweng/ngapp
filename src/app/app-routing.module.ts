a159472f064cc738ee3693bc85139e358c66f89c
Chi-Cheong Weng <chicheongweng@gmail.com>
2019-04-15 11:16:38 +0800

    updated routing, personal, and business

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PersonalComponent } from './personal/personal.component';
+import { BusinessComponent } from './business/business.component';
+import { HelpComponent } from './help/help.component';
+import { DownloadpaymeComponent } from './downloadpayme/downloadpayme.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: '/personal', pathMatch: 'full' },
+  { path: 'personal', component: PersonalComponent },
+  { path: 'business', component: BusinessComponent },
+  { path: 'help', component: HelpComponent },
+  { path: 'download', component: DownloadpaymeComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
